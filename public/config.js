31fb05385418bc587c733ef47d4f335a619755a1
markBeen5 <freschoba@icloud.com>
2026-08-30 20:27:43 -0400

    Update config.js

--- public/config.js
+++ public/config.js
@@ -1,2 +1,2 @@
-window.MARKBEEN5_CONFIG={SUPABASE_URL:'https://wifkhdvmuiioisetzqfr.supabase.co',SUPABASE_PUBLISHABLE_KEY:'sb_publishable_Ot-bQ77UbU-tjFs4WLg1mQ_PudPu9DP''',
+window.MARKBEEN5_CONFIG={SUPABASE_URL:'https://wifkhdvmuiioisetzqfr.supabase.co',SUPABASE_PUBLISHABLE_KEY:'sb_publishable_Ot-bQ77UbU-tjFs4WLg1mQ_PudPu9DP',
 YOUTUBE_API_KEY:'AIzaSyBEHBxAq07a_EREL5EcLcBz4YaCKwvpEqk',};
